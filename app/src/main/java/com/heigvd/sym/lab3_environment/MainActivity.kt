f4f04379cb065c51be4a4a1ec42c22ca620e125b
david-pellissier <dav.pellissier@gmail.com>
2021-11-23 14:05:06 +0100

    mainActivity logic

--- app/src/main/java/com/heigvd/sym/lab3_environment/MainActivity.kt
+++ app/src/main/java/com/heigvd/sym/lab3_environment/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.heigvd.sym.lab3_environment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
+
+    private lateinit var btnBarcode: Button
+    private lateinit var btnNFC: Button
+    private lateinit var btniBeacon: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnBarcode = findViewById(R.id.main_btn_barcode)
+        btniBeacon = findViewById(R.id.main_btn_ibeacon)
+        btnNFC = findViewById(R.id.main_btn_nfc)
+
+        btnBarcode.setOnClickListener {
+            val intent = Intent(this, Barcode::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
+
+        btniBeacon.setOnClickListener {
+            val intent = Intent(this, iBeacon::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
+
+        btnNFC.setOnClickListener {
+            val intent = Intent(this, NFC::class.java)
+            startActivity(intent)
+
+            return@setOnClickListener
+        }
+
+
     }
 }
