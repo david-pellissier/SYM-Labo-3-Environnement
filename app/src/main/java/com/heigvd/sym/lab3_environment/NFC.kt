9fbd6d718e4b6a70fb7a7cce050f529219766c5a
rya-sge <ryan.sauge@heig-vd.ch>
2021-11-23 16:10:53 +0100

    Add pop-up base

--- app/src/main/java/com/heigvd/sym/lab3_environment/NFC.kt
+++ app/src/main/java/com/heigvd/sym/lab3_environment/NFC.kt
@@ -2,10 +2,21 @@ package com.heigvd.sym.lab3_environment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class NFC : AppCompatActivity() {
+    private var AUTHENTICATE_MAX = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc)
+
+
+
+        //Pop-up
+        val text = "Durée de vie"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
     }
 }
