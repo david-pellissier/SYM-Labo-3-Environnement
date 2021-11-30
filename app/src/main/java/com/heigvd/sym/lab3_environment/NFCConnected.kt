b52ce308d48df9d8994f73443b8d372497a00462
david-pellissier <dav.pellissier@gmail.com>
2021-11-30 14:33:33 +0100

    Layout

---
+++ app/src/main/java/com/heigvd/sym/lab3_environment/NFCConnected.kt
@@ -0,0 +1,11 @@
+package com.heigvd.sym.lab3_environment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NFCConnected : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfcconnected)
+    }
+}
