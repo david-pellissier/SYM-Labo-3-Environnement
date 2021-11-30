e6359a960de702ab4d0a8393cc0cbc24e0d1b713
david-pellissier <dav.pellissier@gmail.com>
2021-11-30 14:41:59 +0100

    NFCConnected

--- app/src/main/java/com/heigvd/sym/lab3_environment/NFCConnected.kt
+++ app/src/main/java/com/heigvd/sym/lab3_environment/NFCConnected.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class NFCConnected : AppCompatActivity() {
+
+
+    private var authenticateValue = AUTHENTICATE_MAX
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfcconnected)
+
+
+
+
+    }
+
+    fun updateAuthStatus() {
+
+
+
+    }
+
+
+
+    companion object {
+        private val AUTHENTICATE_MAX = 10
     }
 }
