76526c28286cab5abef31a210a3f340129ae399e
david-pellissier <dav.pellissier@gmail.com>
2021-12-07 14:35:05 +0100

    Implement NFC functionality

--- app/src/main/java/com/heigvd/sym/lab3_environment/NFCConnected.kt
+++ app/src/main/java/com/heigvd/sym/lab3_environment/NFCConnected.kt
@@ -1,13 +1,19 @@
 package com.heigvd.sym.lab3_environment
 
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Toast
-import java.lang.IllegalStateException
+import com.heigvd.sym.lab3_environment.Utils.ForegroundNFC
+import com.heigvd.sym.lab3_environment.Utils.manageNFC
 import java.util.*
 
 class NFCConnected : AppCompatActivity() {
@@ -16,6 +22,8 @@ class NFCConnected : AppCompatActivity() {
     private lateinit var btnMedium: Button
     private lateinit var btnLow: Button
     private lateinit var progressBar: ProgressBar
+
+    private var mNfcAdapter: NfcAdapter? = null
 
     private var authStatus = AUTHENTICATE_INIT
     private val handler = Handler(Looper.getMainLooper())
@@ -52,21 +60,115 @@ class NFCConnected : AppCompatActivity() {
             return@setOnClickListener
         }
         btnLow.setOnClickListener {
-            refreshAuthStatus() // just for debug
-            //checkAuthLevel(AUTHENTICATE_LOW)
+            checkAuthLevel(AUTHENTICATE_LOW)
             return@setOnClickListener
+        }
+
+        //NFC code
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+
+        }else if (!mNfcAdapter!!.isEnabled()) {
+            Toast.makeText(this, "NFC is disabled.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "NFC is enabled", Toast.LENGTH_LONG).show();
+            Log.d("Handle : ", "beginning")
+            handleIntent(intent);
         }
     }
 
     private fun checkAuthLevel(level: Int) {
         val message = if(authStatus > level) AUTH_SUFFICIENT else AUTH_INSUFFICIENT
-        Toast.makeText(this, "$message ($authStatus/$level)", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "$message ($authStatus/$level)", Toast.LENGTH_SHORT).show()
     }
 
     private fun refreshAuthStatus() {
         authStatus = AUTHENTICATE_INIT
         progressBar.setProgress(AUTHENTICATE_MAX, true)
+        Toast.makeText(this, "Authentication refreshed !", Toast.LENGTH_SHORT).show()
     }
+
+    inner class ManageNFCImpl : manageNFC() {
+        @Override
+        override fun onPostExecute(result: String){
+            refreshAuthStatus()
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val action = intent.action
+        Log.e("handleIntent : ", "begin2")
+
+        /*
+        Intent to start an activity when a tag with NDEF payload is discovered.
+         */
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            Log.e(TAG, "begin3")
+            val type = intent.type
+            if (MIME_TEXT_PLAIN == type) {
+                val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+                Log.e(TAG, "handleIntent")
+                this.ManageNFCImpl().execute(tag)
+            } else {
+                Log.e(TAG, "Wrong mime type: $type")
+            }
+            //Intent to start an activity when a tag is discovered.
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED == action) {
+
+            // In case we would still use the Tech Discovered Intent
+            Log.e(TAG, "action-tech-discovers")
+            Toast.makeText(this, "Taction-tech-discovers", Toast.LENGTH_LONG).show();
+            val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+            val techList: Array<String> = tag?.getTechList() as Array<String>
+            val searchedTech = Ndef::class.java.name
+            for (tech in techList) {
+                if (searchedTech == tech) {
+                    manageNFC().execute(tag)
+                    break
+                }
+            }
+        }else{
+            Log.e(TAG, "Nothing")
+        }
+    }
+
+    override fun onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        ForegroundNFC().stopForegroundDispatch(this, mNfcAdapter!!)
+        super.onPause()
+    }
+
+    /*
+    Nécessaire pour pouvoir lire plusieurs fois le tag
+     */
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent!!)
+    }
+
+    override fun onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        mNfcAdapter?.let {  ForegroundNFC().setupForegroundDispatch(this, it) };
+    }
+
 
     companion object {
         private const val AUTHENTICATE_INIT = 120
@@ -79,5 +181,7 @@ class NFCConnected : AppCompatActivity() {
         private const val AUTHENTICATE_DECREASE = 1
         private const val DELAY_MS : Long = 100
         private const val TAG = "NFC/Connected"
+
+        private const val MIME_TEXT_PLAIN = "text/plain"
     }
 }
